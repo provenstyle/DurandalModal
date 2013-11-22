d7d7654dedf8048198c02e7966b2ae0c9b104ef1
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-11-22 15:05:04 -0600

    Initial Commit

---
+++ DurandalModal/App/viewmodels/myModal.js
@@ -0,0 +1,28 @@
+﻿define(['plugins/dialog'], function(dialog) {
+
+   var close = function () {
+      dialog.close(this, 'close');
+   };
+   
+   var yes = function () {
+      dialog.close(this, 'yes');
+   };
+   
+   var no = function () {
+      dialog.close(this, 'no');
+   };
+
+   var message = ko.observable('');
+
+   var activate = function(data) {
+      message(data);
+   };
+
+   return {
+      activate: activate,
+      close: close,
+      yes:yes,
+      no: no,
+      message:message
+   };
+});
