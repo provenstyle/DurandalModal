3a497ba42fc4c45f7c025164a0400b8baa9f0c51
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-12-01 19:30:57 -0600

    Adding message box demo

--- DurandalModal/App/viewmodels/shell.js
+++ DurandalModal/App/viewmodels/shell.js
@@ -3,8 +3,9 @@
         router: router,
         activate: function () {
             router.map([
-                { route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
-                { route: 'PassingData', title:'Passing Data', moduleId: 'viewmodels/passingData', nav: true }
+                { route: ['Welcome',''], title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
+                { route: 'PassingData', title:'Passing Data', moduleId: 'viewmodels/passingData', nav: true },
+                { route: 'MessageBox', title:'Message Box', moduleId: 'viewmodels/messageBox', nav: true }
             ]).buildNavigationModel();
             
             return router.activate();
